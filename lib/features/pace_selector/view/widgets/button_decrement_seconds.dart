06e0cfd2fe09c62635298dcb3cef7890d5ef996e
Illia Prudkyi <ilya.prudkiy20@gmail.com>
2026-06-21 00:31:39 +0200

    Add seconds decrement button

---
+++ lib/features/pace_selector/view/widgets/button_decrement_seconds.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ButtonDecrementSecondsWidget extends StatelessWidget {
+  const ButtonDecrementSecondsWidget({
+    super.key,
+    required this.onDecrementSeconds,
+  });
+
+  final VoidCallback onDecrementSeconds;
+
+  @override
+  Widget build(BuildContext context) {
+    return Ink(
+      width: 40,
+      height: 40,
+      decoration: ShapeDecoration(
+        shape: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
+        color: Colors.white60.withValues(alpha: 0.10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(right: 7),
+        child: Center(
+          child: IconButton(
+            onPressed: () {
+              onDecrementSeconds;
+            },
+            icon: Icon(Icons.keyboard_arrow_down_outlined),
+          ),
+        ),
+      ),
+    );
+  }
+}
