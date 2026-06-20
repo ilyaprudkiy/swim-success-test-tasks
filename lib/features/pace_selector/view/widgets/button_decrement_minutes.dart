bd60474d5356c38f8aed074651abdef813b17b20
Illia Prudkyi <ilya.prudkiy20@gmail.com>
2026-06-21 00:32:50 +0200

    Add minutes decrement button

---
+++ lib/features/pace_selector/view/widgets/button_decrement_minutes.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ButtonDecrementMinutesWidget extends StatelessWidget {
+  const ButtonDecrementMinutesWidget({
+    super.key,
+    required this.onDecrementMinutes,
+  });
+
+  final VoidCallback onDecrementMinutes;
+
+  @override
+  Widget build(BuildContext context) {
+    return Ink(
+      width: 40,
+      height: 40,
+      decoration: ShapeDecoration(
+        shape: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
+        color: Colors.white60.withValues(alpha: 0.10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(right: 7),
+        child: Center(
+          child: IconButton(
+            onPressed: () {
+              onDecrementMinutes;
+            },
+            icon: Icon(Icons.keyboard_arrow_down_outlined),
+          ),
+        ),
+      ),
+    );
+  }
+}
